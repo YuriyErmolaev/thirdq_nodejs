aaecc316a253adb9f402d31468778d7458586f9a
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2022-01-03 11:13:20 +0700

    add counts realisation

--- index.js
+++ index.js
@@ -1,49 +1,29 @@
-const colors = require("colors/safe");
-let [num1, num2] = process.argv.slice(2);
-num1 = +num1; num2 = +num2;
+console.log('Record 1');
 
-if(
-    !Number.isInteger(num1) ||
-    !Number.isInteger(num2)
-){
-    console.log(colors.red('Some of arguments is not a number.'));
-    throw '';
-}
+setTimeout(() => {
+  
+  console.log('Record 2');
+  
+  Promise.resolve().then(() => {
+    
+    setTimeout(() => {
+    
+        console.log('Record 3');
+    
+        Promise.resolve().then(() => {
+            console.log('Record 4');
+        }); 
 
-if(num2 < num1){
-    console.log(colors.red('Second number is less than first'));
-    throw '';
-}
+    });
 
-const getPrimes = (max) => {    
-    let sieve = [], i, j, primes = [];
-    for (i = 2; i <= max; ++i) {
-        if (!sieve[i]) {
-            // i has not been marked -- it is prime
-            primes.push(i);
-            for (j = i << 1; j <= max; j += i) {
-                sieve[j] = true;
-            }
-        }
-    }
-    return primes;
-}
+  });
 
-let primes = getPrimes(num2);
-
-let count = 0;
-let hasPrimes = false;
-primes.forEach(num => {
-    if(num>=num1){
-        count++;
-        switch(count){
-            case 1: console.log(colors.green(num)); break;
-            case 2: console.log(colors.yellow(num)); break;
-            case 3: console.log(colors.red(num)); break;
-            default: count = 0;
-        }
-        hasPrimes = true;
-    }    
 });
 
-if(!hasPrimes) console.log(colors.red('There is no prime numbers in range.'));
+console.log('Record 5');
+
+Promise.resolve().then(
+    () => Promise.resolve().then(
+        () => console.log('Record 6')
+    )
+);
